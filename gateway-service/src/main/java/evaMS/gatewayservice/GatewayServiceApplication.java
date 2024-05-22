04ce51d88af4deabb1e955ef23450be2866f3795
Poke Martinez <david.martinez.e@usach.cl>
2024-05-22 20:36:33 +0200

    Avance

--- gateway-service/src/main/java/evaMS/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/evaMS/gatewayservice/GatewayServiceApplication.java
@@ -1,11 +1,9 @@
-package com.tutorial.gatewayservice;
+package evaMS.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
--- gateway-service/src/test/java/evaMS/gatewayservice/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/evaMS/gatewayservice/GatewayServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tutorial.gatewayservice;
+package evaMS.gatewayservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
